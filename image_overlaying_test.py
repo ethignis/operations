73f91b0069da6bd2453d161b221d52855d383450
bizoffermark <bizoffermark@gmail.com>
2021-05-31 20:25:24 +0200

    mounted images

--- image_overlaying_test.py
+++ image_overlaying_test.py
@@ -5,9 +5,25 @@ import rospy
 import cv2
 
 def main():
+    print("start of programm")
     rospy.init_node("Test",anonymous=True)
     thermal = ThermalCameraSubscriber()
+    print("thermal released")
     visual = VisualSubscriber()
+    print("visual released")
+
+    save=True
+    print(save)
+    if save == True:
+        print("start saving")
+        thermal_img = thermal.get_thermal_image()
+        print("thermal is fine")
+        visual_img = visual.get_visual_image()
+        print("visual is fine")
+        shot = 7   
+        cv2.imwrite("data/mounted_images/thermal-{}.jpg".format(shot), thermal_img)
+        cv2.imwrite("data/mounted_images/visual-{}.jpg".format(shot), visual_img)
+        return
     while True:
         thermal_img = thermal.get_thermal_image()
         visual_img = visual.get_visual_image()
